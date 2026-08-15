0f7ce4e54b1116a34e606f887bc50aa8820edf3f
Shurickin <jordanlevy03@gmail.com>
2026-08-14 20:39:58 -0400

    minor fix in import of ChatData

--- src/chatbot-ui/src/views/Home.jsx
+++ src/chatbot-ui/src/views/Home.jsx
@@ -8,7 +8,7 @@ import Sidebar from '../components/Sidebar'
 import ChatInput from "../components/ChatInput";
 import ChatMessages from "../components/ChatMsgs";
 import dropdown from "../assets/dropdown.png";
-import {settingsSidebar} from '../components/chatData';
+import {settingsSidebar} from '../components/ChatData';
 
 // Put these smaller components above your main Home component
 const GeneralPanel = () => (
